aca0ecd392998775cd7dcfa3cbed5cd4ecc64cb8
ahmedjhin <a7madti90@hotmail.com>
2023-03-08 04:36:21 +0400

    setup1

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse 
 
 # Create your views here.
+
+
+def index(request,name):
+    return render(request,'hello/index.html',{'name':name})
+
+
+
+
+def greet(request,name):
+    return render(request, 'hello/name.html',{'name':name})
